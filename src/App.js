bb856f222cabe29e377613cb503aab6c5877aaa0
Elise0419 <144296227+Elise0419@users.noreply.github.com>
2023-09-12 15:04:04 +0800

    9/12 3:04

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Home from "./component/Home";
-import Love from "./component/Love";
 import Upload from "./component/Upload";
 import Post from "./component/Post";
 import Login from "./component/Login";
 import Signup from "./component/Signup";
+import Fashion from "./component/Theme/Fashion";
+import Love from "./component/Theme/Love";
+import Health from "./component/Theme/Health";
+import Food from "./component/Theme/Food";
+import Makeup from "./component/Theme/Makeup";
 
 class App extends Component {
   state = {};
@@ -14,12 +18,16 @@ class App extends Component {
     return (
       <BrowserRouter>
         <Switch>
-          <Route path="/" component={Home} exact />
-          <Route path="/love" component={Love} exact />
-          <Route path="/upload" component={Upload} exact />
+          <Route path="/" component={Home} />
+          <Route path="/upload" component={Upload} />
           <Route path="/post" component={Post} />
           <Route path="/signup" component={Signup} />
           <Route path="/login" component={Login} />
+          <Route path="/fashion" component={Fashion} />
+          <Route path="/love" component={Love} />
+          <Route path="/health" component={Health} />
+          <Route path="/food" component={Food} />
+          <Route path="/makeup" component={Makeup} />
         </Switch>
       </BrowserRouter>
     );

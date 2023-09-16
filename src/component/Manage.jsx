503f7425d8db72533f464e472b0cd48ae839c357
Doris.Chi <88921212+doris-best@users.noreply.github.com>
2023-09-17 06:40:52 +0800

    9/17 manage/aside finish

--- src/component/Manage.jsx
+++ src/component/Manage.jsx
@@ -33,13 +33,12 @@ function Manage() {
                 發布貼文
               </TabSelector>
               <TabSelector
-              className="manageTab"
+                className="manageTab"
                 isActive={selectedTab === "collectArticle"}
                 onClick={() => setSelectedTab("collectArticle")}
               >
                 收藏貼文
               </TabSelector>
-
             </nav>
           </div>
           <div>
@@ -83,25 +82,31 @@ function Manage() {
             <TabPanel hidden={selectedTab !== "collectArticle"}>
               <div>收藏貼文</div>{" "}
             </TabPanel>
-
           </div>
         </div>
       </article>
+      
       <aside>
-        
         {/* 側邊欄內容 */}
         <div className="aside">
-          <img className="userImg" src={user} alt="" />
-          <h3>David.one</h3>
-          <hr />
-          <span>致力於打造美好生活</span>
-          <p>創建時間:2023-08-01</p>
+          <p className="forumUser">
+            <img className="userImg" src={user} alt="" />
+            <h3>David.one</h3>
+          </p>
+          <hr color="lightgray" width="80%" />
+          <div className="asideInfo">
+            <span>致力於打造美好生活,致力於打造美好生活,致力於打造美好生活</span>
+            <p>創建時間:2023-08-01</p>
+          </div>
+
           <div className="postNumber">
             <span>03</span>
             <span>當前貼文數量</span>
+            <button className="creatPost">創建貼文</button>
           </div>
-          <button className="creatPost">創建貼文</button>
+
         </div>
+
       </aside>
       <Footer />
     </div>

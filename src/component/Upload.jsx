de9c7febcd1f6e2cb14ac1f17158b433824bf782
Elise0419 <144296227+Elise0419@users.noreply.github.com>
2023-09-18 09:12:56 +0800

    9/18

--- src/component/Upload.jsx
+++ src/component/Upload.jsx
@@ -1,51 +1,120 @@
-import React, { Component, useState } from "react";
-import axios from "axios";
+import React, { Component } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import "quill/dist/quill.core.css";
 
 import "./Upload.css";
 import Header from "./Block/Header";
 import Footer from "./Block/Footer";
-import Quill from "./Quill";
 
-// import { CKEditor } from "@ckeditor/ckeditor5-react";
-// import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+class Quill extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "",
+    };
+  }
 
-function Upload() {
-  // var [article, setArticle] = useState("");
-  // async function send() {
-  //   var url = "http://10.10.247.43:8000/api/createposts";
-  //   var result = await axios.post(url, article);
-  //   alert("Done");
-  // }
-  return (
-    <div>
-      <Header />
-      {/* <CKEditor
-        editor={ClassicEditor}
-        data=""
-        // onReady={(editor) => {
-        //   // You can store the "editor" and use when it is needed.
-        //   console.log("Editor is ready to use!", editor);
-        // }}
-        onChange={(event, editor) => {
-          const data = editor.getData();
-          // console.log({ event, editor, data });
-          setArticle({ article: data });
-        }}
-        // onBlur={(event, editor) => {
-        //   console.log("Blur.", editor);
-        // }}
-        // onFocus={(event, editor) => {
-        //   console.log("Focus.", editor);
-        // }}
-      /> */}
-      <Quill />
+  // 存文字
+  handleChange = (value) => {
+    this.setState({ text: value });
+  };
+
+  // 清除文字
+  handleReset = () => {
+    this.setState({ text: "" });
+  };
+
+  // 存照片
+  handleImageUpload = () => {
+    const formData = new FormData();
+    formData.append("text", this.state.text);
+
+    // 轉blob的函式
+    const dataURItoBlob = (dataURI) => {
+      const byteString = atob(dataURI.split(",")[1]);
+      const mimeString = dataURI.split(",")[0].split(":")[1].split(";")[0];
+      const ab = new ArrayBuffer(byteString.length);
+      const ia = new Uint8Array(ab);
+
+      for (let i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+      }
+
+      return new Blob([ab], { type: mimeString });
+    };
+
+    // 取得Quill編輯器
+    const quill = this.quillRef.getEditor();
+    // 取得Quill編輯器裡的照片
+    const images = document.querySelectorAll(".quill-editor img");
+
+    // 照片轉blob格式 並存進formData裡
+    images.forEach((image, index) => {
+      const dataURI = image.getAttribute("src");
+      const blob = dataURItoBlob(dataURI);
+      formData.append(`image${index}`, blob, `image${index}.png`);
+    });
+
+    // Laravel
+    fetch("http://192.168.194.32:8000/api/upload", {
+      method: "POST",
+      body: formData,
+      // mode: "no-cors",
+    })
+      .then((response) => {
+        console.log(response);
+        if (response.ok) {
+          // 上傳成功要幹嘛
+        } else {
+          // 上傳失敗要幹嘛
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    // window.location.assign("http://");
+  };
+
+  render() {
+    return (
       <div>
-        <input className="reBtn" type="reset" value="取消" />
-        <input className="upBtn" type="button" value="送出" />
+        <Header />
+        <ReactQuill
+          ref={(el) => {
+            this.quillRef = el;
+          }}
+          className="quill-editor"
+          value={this.state.text}
+          onChange={this.handleChange}
+          modules={{
+            toolbar: [
+              [{ header: "1" }, { header: "2" }],
+              ["bold", "italic", "underline", "strike"],
+              [{ list: "ordered" }, { list: "bullet" }],
+              ["blockquote", "code-block"],
+              [{ align: [] }],
+              ["link", "image"],
+              ["color", "background"],
+              ["clean"],
+              ["code"],
+            ],
+          }}
+        />
+        <button onClick={this.handleImageUpload} className="upBtn">
+          送出
+        </button>
+        <input
+          type="reset"
+          value="重置"
+          onClick={this.handleReset}
+          className="reBtn"
+        />
+        <Footer />
       </div>
-      <Footer />
-    </div>
-  );
+    );
+  }
 }
 
-export default Upload;
+export default Quill;

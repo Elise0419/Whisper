3f616b5468ec0e92a454662926fade35df44459d
Elise0419 <144296227+Elise0419@users.noreply.github.com>
2023-09-19 15:45:35 +0800

    9/19

--- src/component/Secret.jsx
+++ src/component/Secret.jsx
@@ -2,11 +2,15 @@ import React, { Component } from "react";
 
 import Header from "./Block/Header";
 import Footer from "./Block/Footer";
+import love from "./Img/love4.png";
 
 function Secreat() {
   return (
     <div>
       <Header />
+      <div>
+        <img src={love} />
+      </div>
       <Footer />
     </div>
   );

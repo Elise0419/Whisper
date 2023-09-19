a5516e95396285260c0fe37855fa6658454f4204
hooolar <b25297412@gmail.com>
2023-09-19 22:57:36 +0800

    Update custom_verify_email.blade.php
    修改信箱驗證模板

--- laravel-api/resources/views/vendor/mail/custom_verify_email.blade.php
+++ laravel-api/resources/views/vendor/mail/custom_verify_email.blade.php
@@ -13,11 +13,10 @@
                 <table class="content" width="600px" style="background-color: #ffffff; border-radius: 10px; margin: 20px auto; padding: 20px; ">
                     <tr>
                         <td>
-                            <h1 style="color: #333;">验证邮箱地址</h1>
-                            <p>请点击下面的链接验证您的邮箱地址。</p>
+                            <h1 style="color: #333;">驗證信箱地址</h1>
+                            <p>請點擊下方連結完成信箱驗證。</p>
                             <p><a href="{{ $actionUrl }}" style="background-color: #007bff; color: #fff; padding: 10px 20px; text-decoration: none; border-radius: 5px;">验证邮箱</a></p>
-                            <p>如果您未请求此验证，请忽略此邮件。</p>
-                            <p>感谢您使用我们的应用程序！</p>
+                            <p>如果您未申請驗證，請忽略此郵件。</p>
                         </td>
                     </tr>
                 </table>

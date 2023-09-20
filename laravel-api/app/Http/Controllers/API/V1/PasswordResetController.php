18ebc48d6e43e0e2f82899641731f56efc065841
Wuisonstv <find96301@gmail.com>
2023-09-20 15:14:36 +0800

    Update PasswordResetController.php

--- laravel-api/app/Http/Controllers/API/V1/PasswordResetController.php
+++ laravel-api/app/Http/Controllers/API/V1/PasswordResetController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\API\V1;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Notifications\ForgotPasswordEmail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Password;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class PasswordResetController extends Controller
 {

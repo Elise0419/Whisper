f112be6f8516b1ef1435a8d50a657078f49bc543
hooolar <b25297412@gmail.com>
2023-09-22 10:49:40 +0800

    09/22
    login 狀態碼變更
    ip 修改至zero Tier

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -41,7 +41,7 @@ function Login() {
       };
 
       // 只有当表单验证通过时才进行页面跳转
-      fetch("http://127.0.0.1:8000/api/login", {
+      fetch("http://10.147.20.3/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -58,18 +58,17 @@ function Login() {
           // 在这里处理从后端返回的数据
           localStorage.setItem("token", data.authorization.token); // 将Token存储在本地
           const token = localStorage.getItem("token");
-          if (data.status === 403) {
-            history.push("/verify", { token: data.authorization.token });
-          }
-
           // 将Token存储在本地
           console.log("Token:", token);
-
-          // history.push("/", { token: data.authorization.token });
-          // 登录成功后跳转到首页
-
-          history.push("/profile", { token: data.authorization.token });
-          console.log("successfullogin");
+          if (data.status >= 400) {
+            // 400 切換至驗證email
+            history.push("/verify", { token: data.authorization.token });
+          } else if (data.status >= 200) {
+            // history.push("/", { token: data.authorization.token });
+            // 登录成功后跳转到首页
+            history.push("/profile", { token: data.authorization.token });
+            console.log("successfullogin");
+          }
         })
 
         .catch((error) => {

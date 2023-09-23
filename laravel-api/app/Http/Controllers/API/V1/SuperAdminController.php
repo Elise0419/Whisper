6c7a8d9a9212c4a7807a8eae05c45a39715264b4
hooolar <b25297412@gmail.com>
2023-09-24 00:42:15 +0800

    Create SuperAdminController.php

---
+++ laravel-api/app/Http/Controllers/API/V1/SuperAdminController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class SuperAdminController extends Controller
+{
+    //
+
+    public function showarticle(Request $req)
+    {
+    }
+}

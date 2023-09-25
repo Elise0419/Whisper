8c33e1394eef6dff519ae287c4671e50265eaa85
Doris.Chi <88921212+doris-best@users.noreply.github.com>
2023-09-25 11:18:21 +0800

    9/25 asideuser

--- src/component/Block/Asideuser.jsx
+++ src/component/Block/Asideuser.jsx
@@ -4,29 +4,52 @@ import "../CSS/Asideuser.css";
 import rabbit from "../img/rabbit.png";
 
 function Asideuser() {
-  let [user, setUser] = useState([]);
+  const [user, setUser] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [postCount, setPostCount] = useState(0);
+
   let dTSc = user.created_at;
   let dTc = new Date(dTSc);
   let yc = dTc.getFullYear();
-  let mc = (dTc.getMonth() + 1).toString().padStart(2, "0"); // 注意月份从 0 开始，需要加 1
+  let mc = (dTc.getMonth() + 1).toString().padStart(2, "0");
   let dc = dTc.getDate().toString().padStart(2, "0");
   let fDc = `${yc}-${mc}-${dc}`;
 
   let dTSu = user.updated_at;
   let dTu = new Date(dTSu);
   let yu = dTu.getFullYear();
-  let mu = (dTu.getMonth() + 1).toString().padStart(2, "0"); // 注意月份从 0 开始，需要加 1
+  let mu = (dTu.getMonth() + 1).toString().padStart(2, "0");
   let du = dTu.getDate().toString().padStart(2, "0");
   let fDu = `${yu}-${mu}-${du}`;
 
-  const token = localStorage.getItem("token");
-  console.log("Token in Profile:", token);
+  const fetchPosts = () => {
+    const token = localStorage.getItem("token");
+    fetch(`http://10.10.247.90:8000/api/user/posts`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((resJson) => {
+        if (resJson.message === "没有蒐藏任何貼文") {
+          setPosts([]);
+          setPostCount(0);
+        } else {
+          setPosts(resJson.posts);
+          setPostCount(resJson.postCount);
+        }
+      })
+      .catch((error) => console.error("Error:", error));
+  };
 
   useEffect(() => {
     function fetchData() {
-      fetch("http://10.10.247.90:8000/api/login", {
-        method: "POST",
+      const token = localStorage.getItem("token");
+      fetch("http://10.10.247.90:8000/api/profile", {
         headers: {
+          "Content-Type": "application/json",
           Authorization: `Bearer ${token}`,
         },
       })
@@ -45,70 +68,44 @@ function Asideuser() {
           }
         })
         .catch((err) => {
-          console.log("Error:", err);
-        });
-
-      fetch(`http://127.0.0.1:8000/api/profile`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      })
-        .then((res) => {
-          return res.json();
-        })
-        .then((jsonData) => {
-          setUser(jsonData.user);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-
-      fetch(`http://10.10.247.90:8000/api/profile`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      })
-        .then((res) => {
-          return res.json();
-        })
-        .then((jsonData) => {
-          setUser(jsonData.user);
-        })
-        .catch((error) => {
-          console.log(error);
+          console.log("錯誤:", err);
         });
     }
 
     fetchData();
-
+    fetchPosts();
   }, []);
 
   return (
     <div className="aside">
-      <img className="asideImg" src={user.headimg || rabbit} />
-      <span className="asideName">{user.mem_name}</span>
-      <hr />
-      <div className="asideUser">
-      <span className="asideMsg">{user.promise}</span>
-      <br />
-       <span className="asideTime">創建時間: {fDc}</span>
-      <br />
-      <span className="asideTime">最後更新時間:2023-08-01</span>
-      <div className="asideNum">
-        {/* <p>{postCount}</p> */}
-        <p>發布貼文數量</p>
-      </div>
-      <Link to="/upload/life">
-      <span className="asideTime">最後更新時間: {fDu}</span>
-      </Link>
-    </div>
+      <div className="asideContainer">
+        <div className="asideName">
+          <div>{console.log("user", user)}</div>
+          <img className="asideImg" src={user.headimg} />
+          <span className="asideText">{user.mem_name}</span>
+        </div>
 
+        <div className="asideUser">
+          <hr />
+
+          <div className="asideMsg">{user.promise}</div>
+          <br />
+
+          <div className="asideTime">
+            <span>創建時間: {fDc}</span>
+            <span>最後更新時間: {fDu}</span>
+          </div>
+
+          <div>
+            <p className="asideNum">{postCount}</p>
+            <p className="asideNum">發布貼文數量</p>
+          </div>
+          <Link to="/upload/fashion">
+            <button className="asideBtn">創建貼文</button>
+          </Link>
+        </div>
       </div>
-      
+    </div>
   );
 }
 

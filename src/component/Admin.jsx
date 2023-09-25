968ee8809654f466b6dd03b022dec10492b4d846
Elise0419 <144296227+Elise0419@users.noreply.github.com>
2023-09-25 15:33:35 +0800

    9/25

--- src/component/Admin.jsx
+++ src/component/Admin.jsx
@@ -1,72 +1,72 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-
 function Admin() {
-    const [data, setData] = useState([]);
-    const [page, setPage] = useState(1);
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
 
-    const token = localStorage.getItem("token");
+  const token = localStorage.getItem("token");
 
-    const Prepage = () => {
-        setPage(page + 1);
-      };
-    const Nextpage = () => {
-        setPage(page + 1);
-      };
+  const Prepage = () => {
+    setPage(page + 1);
+  };
+  const Nextpage = () => {
+    setPage(page + 1);
+  };
 
-    useEffect(() => {
-        fetch(`http://10.10.247.90:8000/api/admin/management/articles/show/${page}`, {
-            method: "get",
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        })
-            .then((res) => {
-                if (res.status >= 400) {
-                    alert('你無權造訪此頁面')
-                    throw new Error("API request failed");
-                } else
-                    return res.json();
-
-            })
-            .then((res) => {
-                setData(res.articles.data);
-            })
-            .catch((error) => {
-                console.error('發生錯誤：', error);
-            });
-    }, []);
-
-    return (
-        <div>
-            <h1>文章資訊</h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th>貼文者</th>
-                        <th>貼文標題</th>
-                        <th>貼文內容</th>
-                        <th>執行操作</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {data.map((item, index) => (
-                        <tr key={item.post_id}>
-                            <td>{item.title}</td>
-                            <td>{item.name}</td>
-                            <td>{item.content}</td>
-                            <td>
-                                <Link to={`/post_${item.post_id}/comments`}>文章評論</Link>
-                                <button>Delete</button>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-            <span  onClick={() => Prepage}>上一頁</span>
-            <span  onClick={() => Nextpage}>下一頁</span>
-        </div>
+  useEffect(() => {
+    fetch(
+      `http://10.10.247.90:8000/api/admin/management/articles/show/${page}`,
+      {
+        method: "get",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
     )
+      .then((res) => {
+        if (res.status >= 400) {
+          alert("你無權造訪此頁面");
+          throw new Error("API request failed");
+        } else return res.json();
+      })
+      .then((res) => {
+        setData(res.articles.data);
+      })
+      .catch((error) => {
+        console.error("發生錯誤：", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>文章資訊</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>貼文者</th>
+            <th>貼文標題</th>
+            <th>貼文內容</th>
+            <th>執行操作</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={item.post_id}>
+              <td>{item.title}</td>
+              <td>{item.name}</td>
+              <td>{item.content}</td>
+              <td>
+                <Link to={`/post_${item.post_id}/comments`}>文章評論</Link>
+                <button>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <span onClick={() => Prepage}>上一頁</span>
+      <span onClick={() => Nextpage}>下一頁</span>
+    </div>
+  );
 }
 export default Admin;
